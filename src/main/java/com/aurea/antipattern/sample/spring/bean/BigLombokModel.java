f68959cc8827b879321c9ff78e3d28f245de2900
Maciste Saldanha <maciste.saldanha@aurea.com>
2017-03-27 15:15:59 -0300

    Added lombok model

---
+++ src/main/java/com/aurea/antipattern/sample/spring/bean/BigLombokModel.java
@@ -0,0 +1,33 @@
+package com.aurea.antipattern.sample.spring.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BigLombokModel {
+    private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+    private String field5;
+    private String field6;
+    private String field7;
+    private String field8;
+    private String field9;
+    private String field10;
+    private String field11;
+    private String field12;
+    private String field13;
+    private String field14;
+    private String field15;
+    private String field16;
+    private String field17;
+    private String field18;
+    private String field19;
+    private String field20;
+    private String field21;
+    private String field22;
+    private String field23;
+
+}
